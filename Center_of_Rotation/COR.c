5c61f787a0a3f3e8c3f2f6b3d5e39ed4cfc45aef
Forbes <fot3@pitt.edu>
2017-06-30 12:30:32 -0400

    Updated matrix calculations

--- Center_of_Rotation/COR.c
+++ Center_of_Rotation/COR.c
@@ -2,17 +2,19 @@
 #include "csvparser.h"
 
 int main() {
-///REDO
-//FOR C4 relative to C3 do: Inverse(C4) cross C3
-
-
-
+    int i,j,frame;
 
     char C3_file[]="HomoTransMatrices_C123_-to-Lab.csv"; 
     char C4_file[]="HomoTransMatrices_C4_-to-Lab.csv";
     char C5_file[]="HomoTransMatrices_C5_-to-Lab.csv";
     char C6_file[]="HomoTransMatrices_C6_-to-Lab.csv";
     char C7_file[]="HomoTransMatrices_C7T1_-to-Lab.csv";
+    char C3r_file[]="HomoTransMatrices_Lab-to-C123_.csv"; 
+    char C4r_file[]="HomoTransMatrices_Lab-to-C4_.csv";
+    char C5r_file[]="HomoTransMatrices_Lab-to-C5_.csv";
+    char C6r_file[]="HomoTransMatrices_Lab-to-C6_.csv";
+    char C7r_file[]="HomoTransMatrices_Lab-to-C7T1_.csv";
+
     double** cor(char *, char *);
     double* flexion_all = malloc(4*500*sizeof(double*));
     double *** results_all = malloc(4*sizeof(double**));
@@ -20,33 +22,53 @@ int main() {
     CsvRow *row;
 
     int rownum=5;
-    
+
     dest = fopen("results.csv","wb");
-    
+
 
     CsvParser *csvparser_angles = CsvParser_new("KinematicsMeasurementReport.csv", ",", 0);
-
-    int i=0;
+    frame=0;
     while ((row = CsvParser_getRow(csvparser_angles)) ) {
         const char **rowFields = CsvParser_getFields(row);
-        flexion_all[4*ii]=atof(rowFields[2]);
-        flexion_all[4*ii+1]=atof(rowFields[5]);
-        flexion_all[4*ii+2]=atof(rowFields[8]);
-        flexion_all[4*ii+3]=atof(rowFields[11]);
+        if(frame>1){
+            flexion_all[4*frame]=atof(rowFields[2]);
+            flexion_all[4*frame+1]=atof(rowFields[5]);
+            flexion_all[4*frame+2]=atof(rowFields[8]);
+            flexion_all[4*frame+3]=atof(rowFields[11]);
+        }
         CsvParser_destroy_row(row);
+        frame++;
     }
     CsvParser_destroy(csvparser_angles);
 
-    double ** results = cor(C3_file,C4_file);
+
+    // This takes finds the next frame with an absolute difference > 2 degrees
+    for(i=2;i<frame;i++){
+        for(j=i;j<frame;j++){
+            if(flexion_all[4*i]+2 < flexion_all[4*j]){
+                printf("Frame %d to %d\n",i,j);
+                break;
+            }
+            if(flexion_all[4*i]-2 > flexion_all[4*j]){
+                printf("Frame %d to %d\n",i,j);
+                break;
+            }
+        }
+    }
+
+
+    /*
+
+    double ** results = cor(C3_file,C4r_file);
     results_all[0]=results;
-   
-    results = cor(C4_file,C5_file);
+
+    results = cor(C4_file,C5r_file);
     results_all[1]=results;
-    
-    results = cor(C5_file,C6_file);
+
+    results = cor(C5_file,C6r_file);
     results_all[2]=results;
 
-    results = cor(C6_file,C7_file);
+    results = cor(C6_file,C7r_file);
     results_all[3]=results;
 
     for(int i=0;i<500;i++){
@@ -55,6 +77,7 @@ int main() {
 
         fprintf(dest, "%f,%f,%f, ,%f,%f,%f, ,%f,%f,%f, ,%f,%f,%f\n", results_all[0][i][0],results_all[0][i][1],results_all[0][i][2]) 
     } 
+    */
 
     return 0;
 }
@@ -66,10 +89,14 @@ double** cor(char *file_1, char *file_2){
     int k = 0;
     int frame = 0;
     int type = 1;
-    double bone1_CS[500][3][3];
-    double position1[500][3];
-    double bone2_CS[500][3][3];
-    double position2[500][3];
+    double total;
+    int framecount;
+    double bone1_CS[500][4][4];
+    double bone2_CS[500][4][4];
+    double bone12_CS[500][4][4];
+    double bone12_rot[500][3][3];
+    double position12[500][3];
+
     double phi;
     double t;
     double intersection[3];
@@ -78,7 +105,7 @@ double** cor(char *file_1, char *file_2){
     VECTOR screw_vec;
     CsvRow *row;
 
-   for (i=0; i<500; i++)
+    for (i=0; i<500; i++)
         results[i] = (double *)calloc(3,sizeof(double));
 
     CsvParser *csvparser1 = CsvParser_new(file_1, ",", 0); 
@@ -92,12 +119,7 @@ double** cor(char *file_1, char *file_2){
                 k=0;
                 j++;
             }
-            if(k!=3 && j<3){
-                bone1_CS[frame][j][k]=atof(rowFields[i]);
-            }
-            if(j==3 && k<3){
-                position1[frame][k]=atof(rowFields[i]);
-            }
+            bone1_CS[frame][j][k]=atof(rowFields[i]);
             k++;
         }
         frame++;
@@ -108,7 +130,6 @@ double** cor(char *file_1, char *file_2){
 
     CsvParser *csvparser2 = CsvParser_new(file_2, ",", 0);
     frame=0;
-
     while ((row = CsvParser_getRow(csvparser2)) ) {
         const char **rowFields = CsvParser_getFields(row);
         j=k=0;
@@ -118,13 +139,7 @@ double** cor(char *file_1, char *file_2){
                 k=0;
                 j++;
             }
-
-            if(k!=3 && j<3){
-                bone2_CS[frame][j][k]=atof(rowFields[i]);
-            }
-            if(j==3 && k<3){
-                position2[frame][k]=atof(rowFields[i]);
-            }
+            bone2_CS[frame][j][k]=atof(rowFields[i]);
             k++;
         }
         frame++;
@@ -133,14 +148,38 @@ double** cor(char *file_1, char *file_2){
 
     CsvParser_destroy(csvparser2);
 
-    //At this point all data is loaded into memory in the appropriate format.
-    //Let's output the rotation angles of bone1 relative to bone2
-    for(i=2;i<frame;i++){
-        icr(type,bone1_CS[i],bone2_CS[i],position1[i],position2[i],&phi,&screw_vec,&t,intersection);
-        
-        for(j=0;j<3;j++)
-            results[i-2][j]=intersection[j];
+    // Performing matrix multiplication to transform into C4-C3 reference
+    // This yields B*A. B should be Lab to C_lower
+    // A should be C_higher to Lab
+    for(framecount=0;framecount<frame;framecount++){
+        for(i=0;i<4;i++){
+            for(j=0;j<4;j++){
+                total=0;
+                for(k=0;k<4;k++){
+                    total+=bone1_CS[framecount][k][j]*bone2_CS[framecount][i][k];
+                }
+                bone12_CS[framecount][i][j]=total;
+            }
+        }
     }
 
+    // Split the matrix into its respective rotation matrix and translation vector
+    for(framecount=0;framecount<frame;framecount++){
+        for(i=0;i<4;i++)
+            for(j=0;j<4;j++)
+                if(i<3 && j<3)
+                    bone12_rot[framecount][i][j]=bone12_CS[framecount][i][j];
+                else if(i<3)
+                    position12[framecount][i]=bone12_rot[framecount][i][j];
+    }
+
+    // At this point all data is loaded into memory in the appropriate format.
+    // Let's output the rotation angles of bone1 relative to bone2
+    for(i=1;i<frame;i++){
+        icr(type,bone12_rot[1],bone12_rot[i],position12[1],position12[i],&phi,&screw_vec,&t,intersection);
+
+        for(j=0;j<3;j++)
+            results[i-1][j]=intersection[j];
+    }
     return results;
 }
---
+++ Center_of_Rotation/include.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "mathtools.h"
