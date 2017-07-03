d5b3908d4387a732204db40fc21307efc4ddb186
Forbes <fot3@pitt.edu>
2017-07-03 11:28:22 -0400

    Added file output formatting

--- Center_of_Rotation/COR.c
+++ Center_of_Rotation/COR.c
@@ -2,7 +2,7 @@
 #include "csvparser.h"
 
 int main() {
-    int i,j,frame;
+    int i,j,k,frame;
 
     char C3_file[]="HomoTransMatrices_C123_-to-Lab.csv"; 
     char C4_file[]="HomoTransMatrices_C4_-to-Lab.csv";
@@ -15,16 +15,14 @@ int main() {
     char C6r_file[]="HomoTransMatrices_Lab-to-C6_.csv";
     char C7r_file[]="HomoTransMatrices_Lab-to-C7T1_.csv";
 
-    double** cor(char *, char *);
+    double** cor(char *, char *, int *,int);
     double* flexion_all = malloc(4*500*sizeof(double*));
-    double *** results_all = malloc(4*sizeof(double**));
     FILE *dest;
     CsvRow *row;
-
-    int rownum=5;
+    int *angles;
+    double **results;
 
     dest = fopen("results.csv","wb");
-
 
     CsvParser *csvparser_angles = CsvParser_new("KinematicsMeasurementReport.csv", ",", 0);
     frame=0;
@@ -42,53 +40,140 @@ int main() {
     CsvParser_destroy(csvparser_angles);
 
 
-    // This takes finds the next frame with an absolute difference > 2 degrees
+    // This is C3 relative to C4 
+    fprintf(dest,"C3 to C4, X,Y,Z\n");
+
+    // This finds the next frame with an absolute difference > 2 degrees
+    angles = malloc(2*500*sizeof(int));
+    k=0;
     for(i=2;i<frame;i++){
         for(j=i;j<frame;j++){
             if(flexion_all[4*i]+2 < flexion_all[4*j]){
-                printf("Frame %d to %d\n",i,j);
+                angles[2*k]=i;
+                angles[2*k+1]=j;
+                k++;
                 break;
             }
             if(flexion_all[4*i]-2 > flexion_all[4*j]){
-                printf("Frame %d to %d\n",i,j);
+                angles[2*k]=i;
+                angles[2*k+1]=j;
+                k++;
                 break;
             }
         }
     }
 
+    results = cor(C3_file,C4r_file,angles,k);
+    for(i=0;i<k;i++){
+        fprintf(dest, "%f,%f,%f,%f\n",flexion_all[4*(i+2)],results[i][0],results[i][1],results[i][2]); 
+    }
+    free(results);
+    free(angles);
 
-    /*
 
-    double ** results = cor(C3_file,C4r_file);
-    results_all[0]=results;
+    // This is C4 relative to C5
+    fprintf(dest,"\n\nC4 to C5, X,Y,Z\n");
 
-    results = cor(C4_file,C5r_file);
-    results_all[1]=results;
+    // This finds the next frame with an absolute difference > 2 degrees
+    angles = malloc(2*500*sizeof(int));
+    k=0;
+    for(i=2;i<frame;i++){
+        for(j=i;j<frame;j++){
+            if(flexion_all[4*i+1]+2 < flexion_all[4*j+1]){
+                angles[2*k]=i;
+                angles[2*k+1]=j;
+                k++;
+                break;
+            }
+            if(flexion_all[4*i]-2 > flexion_all[4*j]){
+                angles[2*k]=i;
+                angles[2*k+1]=j;
+                k++;
+                break;
+            }
+        }
+    }
 
-    results = cor(C5_file,C6r_file);
-    results_all[2]=results;
+    results = cor(C4_file,C5r_file,angles,k);
+    for(i=0;i<k;i++){
+        fprintf(dest, "%f,%f,%f,%f\n",flexion_all[4*(i+2)+1],results[i][0],results[i][1],results[i][2]); 
+    }
+    free(results);
+    free(angles);
 
-    results = cor(C6_file,C7r_file);
-    results_all[3]=results;
 
-    for(int i=0;i<500;i++){
-        if(results[i][1]==0.0 && results[i][2]==0.0)
-            break;
 
-        fprintf(dest, "%f,%f,%f, ,%f,%f,%f, ,%f,%f,%f, ,%f,%f,%f\n", results_all[0][i][0],results_all[0][i][1],results_all[0][i][2]) 
-    } 
-    */
+    // This is C5 relative to C6
+    fprintf(dest,"\n\nC5 to C6, X,Y,Z\n");
+
+    // This finds the next frame with an absolute difference > 2 degrees
+    angles = malloc(2*500*sizeof(int));
+    k=0;
+    for(i=2;i<frame;i++){
+        for(j=i;j<frame;j++){
+            if(flexion_all[4*i+2]+2 < flexion_all[4*j+2]){
+                angles[2*k]=i;
+                angles[2*k+1]=j;
+                k++;
+                break;
+            }
+            if(flexion_all[4*i]-2 > flexion_all[4*j]){
+                angles[2*k]=i;
+                angles[2*k+1]=j;
+                k++;
+                break;
+            }
+        }
+    }
+
+    results = cor(C5_file,C6r_file,angles,k);
+    for(i=0;i<k;i++){
+        fprintf(dest, "%f,%f,%f,%f\n",flexion_all[4*(i+2)+2],results[i][0],results[i][1],results[i][2]); 
+    }
+    free(results);
+    free(angles);
+
+
+    // This is C6 relative to C7
+    fprintf(dest,"\n\nC6 to C7, X,Y,Z\n");
+
+    // This finds the next frame with an absolute difference > 2 degrees
+    angles = malloc(2*500*sizeof(int));
+    k=0;
+    for(i=2;i<frame;i++){
+        for(j=i;j<frame;j++){
+            if(flexion_all[4*i+3]+2 < flexion_all[4*j+3]){
+                angles[2*k]=i;
+                angles[2*k+1]=j;
+                k++;
+                break;
+            }
+            if(flexion_all[4*i]-2 > flexion_all[4*j]){
+                angles[2*k]=i;
+                angles[2*k+1]=j;
+                k++;
+                break;
+            }
+        }
+    }
+
+    results = cor(C6_file,C7r_file,angles,k);
+    for(i=0;i<k;i++){
+        fprintf(dest, "%f,%f,%f,%f\n",flexion_all[4*(i+2)+3],results[i][0],results[i][1],results[i][2]); 
+    }
+    free(results);
+    free(angles);
 
     return 0;
 }
 
 
-double** cor(char *file_1, char *file_2){
+double** cor(char *file_1, char *file_2, int *angles, int count){
     int i = 0;
     int j = 0;
     int k = 0;
     int frame = 0;
-    int type = 1;
+    int type = 3;
     double total;
     int framecount;
     double bone1_CS[500][4][4];
@@ -163,23 +248,29 @@ double** cor(char *file_1, char *file_2){
         }
     }
 
+
     // Split the matrix into its respective rotation matrix and translation vector
     for(framecount=0;framecount<frame;framecount++){
         for(i=0;i<4;i++)
-            for(j=0;j<4;j++)
-                if(i<3 && j<3)
+            for(j=0;j<4;j++){
+                if(i<3 && j<3){
                     bone12_rot[framecount][i][j]=bone12_CS[framecount][i][j];
-                else if(i<3)
-                    position12[framecount][i]=bone12_rot[framecount][i][j];
+                }
+                else if(j<3 && i==3){
+                    position12[framecount][j]=bone12_CS[framecount][i][j];
+                }
+            }
     }
 
     // At this point all data is loaded into memory in the appropriate format.
     // Let's output the rotation angles of bone1 relative to bone2
-    for(i=1;i<frame;i++){
-        icr(type,bone12_rot[1],bone12_rot[i],position12[1],position12[i],&phi,&screw_vec,&t,intersection);
+    
+    for(i=0;i<count;i++){
+        icr(type,bone12_rot[(angles[2*i]-1)],bone12_rot[(angles[2*i+1]-1)],position12[(angles[2*i]-1)], position12[(angles[2*i+1]-1)],&phi,&screw_vec,&t,intersection);
 
-        for(j=0;j<3;j++)
-            results[i-1][j]=intersection[j];
+        for(j=0;j<3;j++){
+            results[i][j]=intersection[j];
+        }
     }
     return results;
 }
