3edd2f9bc7254d6e36cbaa847b0325bce4e129bb
Forbes <fot3@pitt.edu>
2017-07-12 09:52:06 -0400

    Added inline inverse matrices calculations

--- Center_of_Rotation/COR.c
+++ Center_of_Rotation/COR.c
@@ -1,6 +1,9 @@
 #include "include.h"
 #include "csvparser.h"
 
+double Determinant(double **,int);
+void CoFactor();
+
 int main() {
     int i,j,k,frame;
 
@@ -9,14 +12,11 @@ int main() {
     char C5_file[]="HomoTransMatrices_C5_-to-Lab.csv";
     char C6_file[]="HomoTransMatrices_C6_-to-Lab.csv";
     char C7_file[]="HomoTransMatrices_C7T1_-to-Lab.csv";
-    char C3r_file[]="HomoTransMatrices_Lab-to-C123_.csv"; 
-    char C4r_file[]="HomoTransMatrices_Lab-to-C4_.csv";
-    char C5r_file[]="HomoTransMatrices_Lab-to-C5_.csv";
-    char C6r_file[]="HomoTransMatrices_Lab-to-C6_.csv";
-    char C7r_file[]="HomoTransMatrices_Lab-to-C7T1_.csv";
 
-    double** cor(char *, char *, int *,int);
+    double** cor(char *, char *,int *,int);
     double* flexion_all = malloc(4*500*sizeof(double*));
+
+
     FILE *dest;
     CsvRow *row;
     int *angles;
@@ -48,13 +48,14 @@ int main() {
     k=0;
     for(i=2;i<frame;i++){
         for(j=i;j<frame;j++){
-            if(flexion_all[4*i]+2 < flexion_all[4*j]){
+            if(flexion_all[4*i]+4 < flexion_all[4*j]){
                 angles[2*k]=i;
                 angles[2*k+1]=j;
+                //printf("\nAngles are %d\t%d",i,j);
                 k++;
                 break;
             }
-            if(flexion_all[4*i]-2 > flexion_all[4*j]){
+            if(flexion_all[4*i]-4 > flexion_all[4*j]){
                 angles[2*k]=i;
                 angles[2*k+1]=j;
                 k++;
@@ -63,7 +64,7 @@ int main() {
         }
     }
 
-    results = cor(C3_file,C4r_file,angles,k);
+    results = cor(C3_file,C4_file,angles,k);
     for(i=0;i<k;i++){
         fprintf(dest, "%f,%f,%f,%f\n",flexion_all[4*(i+2)],results[i][0],results[i][1],results[i][2]); 
     }
@@ -74,18 +75,18 @@ int main() {
     // This is C4 relative to C5
     fprintf(dest,"\n\nC4 to C5, X,Y,Z\n");
 
-    // This finds the next frame with an absolute difference > 2 degrees
+    // This finds the next frame with an absolute difference > 4 degrees
     angles = malloc(2*500*sizeof(int));
     k=0;
     for(i=2;i<frame;i++){
         for(j=i;j<frame;j++){
-            if(flexion_all[4*i+1]+2 < flexion_all[4*j+1]){
+            if(flexion_all[4*i+1]+4 < flexion_all[4*j+1]){
                 angles[2*k]=i;
                 angles[2*k+1]=j;
                 k++;
                 break;
             }
-            if(flexion_all[4*i]-2 > flexion_all[4*j]){
+            if(flexion_all[4*i]-4 > flexion_all[4*j]){
                 angles[2*k]=i;
                 angles[2*k+1]=j;
                 k++;
@@ -94,7 +95,7 @@ int main() {
         }
     }
 
-    results = cor(C4_file,C5r_file,angles,k);
+    results = cor(C4_file,C5_file,angles,k);
     for(i=0;i<k;i++){
         fprintf(dest, "%f,%f,%f,%f\n",flexion_all[4*(i+2)+1],results[i][0],results[i][1],results[i][2]); 
     }
@@ -111,13 +112,13 @@ int main() {
     k=0;
     for(i=2;i<frame;i++){
         for(j=i;j<frame;j++){
-            if(flexion_all[4*i+2]+2 < flexion_all[4*j+2]){
+            if(flexion_all[4*i+2]+4 < flexion_all[4*j+2]){
                 angles[2*k]=i;
                 angles[2*k+1]=j;
                 k++;
                 break;
             }
-            if(flexion_all[4*i]-2 > flexion_all[4*j]){
+            if(flexion_all[4*i]-4 > flexion_all[4*j]){
                 angles[2*k]=i;
                 angles[2*k+1]=j;
                 k++;
@@ -126,7 +127,7 @@ int main() {
         }
     }
 
-    results = cor(C5_file,C6r_file,angles,k);
+    results = cor(C5_file,C6_file,angles,k);
     for(i=0;i<k;i++){
         fprintf(dest, "%f,%f,%f,%f\n",flexion_all[4*(i+2)+2],results[i][0],results[i][1],results[i][2]); 
     }
@@ -142,13 +143,13 @@ int main() {
     k=0;
     for(i=2;i<frame;i++){
         for(j=i;j<frame;j++){
-            if(flexion_all[4*i+3]+2 < flexion_all[4*j+3]){
+            if(flexion_all[4*i+3]+4 < flexion_all[4*j+3]){
                 angles[2*k]=i;
                 angles[2*k+1]=j;
                 k++;
                 break;
             }
-            if(flexion_all[4*i]-2 > flexion_all[4*j]){
+            if(flexion_all[4*i]-4 > flexion_all[4*j]){
                 angles[2*k]=i;
                 angles[2*k+1]=j;
                 k++;
@@ -157,7 +158,7 @@ int main() {
         }
     }
 
-    results = cor(C6_file,C7r_file,angles,k);
+    results = cor(C6_file,C7_file,angles,k);
     for(i=0;i<k;i++){
         fprintf(dest, "%f,%f,%f,%f\n",flexion_all[4*(i+2)+3],results[i][0],results[i][1],results[i][2]); 
     }
@@ -174,19 +175,20 @@ double** cor(char *file_1, char *file_2, int *angles, int count){
     int k = 0;
     int frame = 0;
     int type = 3;
-    double total;
     int framecount;
-    double bone1_CS[500][4][4];
-    double bone2_CS[500][4][4];
-    double bone12_CS[500][4][4];
+    double bone1_rot[500][3][3];
+    double bone2_rot[500][3][3];
+    double bone2_invrot[500][3][3];
+    double position1[500][3];
+    double position2[500][3];
     double bone12_rot[500][3][3];
     double position12[500][3];
-
     double phi;
     double t;
     double intersection[3];
     double** results = malloc(500*sizeof(double*));
     FILE *dest;
+
     VECTOR screw_vec;
     CsvRow *row;
 
@@ -199,12 +201,16 @@ double** cor(char *file_1, char *file_2, int *angles, int count){
         const char **rowFields = CsvParser_getFields(row);
         j=k=0;
         for (i = 2 ; i < CsvParser_getNumFields(row) ; i++) {
-            //Need to filter out every fourth column beginning with first.
             if(k==4){
                 k=0;
                 j++;
             }
-            bone1_CS[frame][j][k]=atof(rowFields[i]);
+            if(j==3 && k!=3){
+                position1[frame][k]=atof(rowFields[i]);
+            }
+            else if(k!=3){
+                bone1_rot[frame][j][k]=atof(rowFields[i]);
+            }
             k++;
         }
         frame++;
@@ -212,10 +218,9 @@ double** cor(char *file_1, char *file_2, int *angles, int count){
     }
     CsvParser_destroy(csvparser1);
 
-
-    CsvParser *csvparser2 = CsvParser_new(file_2, ",", 0);
+    CsvParser *csvparser3 = CsvParser_new(file_2, ",", 0);
     frame=0;
-    while ((row = CsvParser_getRow(csvparser2)) ) {
+    while ((row = CsvParser_getRow(csvparser3)) ) {
         const char **rowFields = CsvParser_getFields(row);
         j=k=0;
         for (i = 2 ; i < CsvParser_getNumFields(row) ; i++) {
@@ -224,53 +229,151 @@ double** cor(char *file_1, char *file_2, int *angles, int count){
                 k=0;
                 j++;
             }
-            bone2_CS[frame][j][k]=atof(rowFields[i]);
+            if(j==3 && k!=3){
+                position2[frame][k]=atof(rowFields[i]);
+            }
+            else if(j<3 && k!=3){
+                bone2_invrot[frame][j][k]=atof(rowFields[i]);
+            }
             k++;
         }
         frame++;
         CsvParser_destroy_row(row);
     }
+    CsvParser_destroy(csvparser3);
 
-    CsvParser_destroy(csvparser2);
-
-    // Performing matrix multiplication to transform into C4-C3 reference
-    // This yields B*A. B should be Lab to C_lower
-    // A should be C_higher to Lab
-    for(framecount=0;framecount<frame;framecount++){
-        for(i=0;i<4;i++){
-            for(j=0;j<4;j++){
-                total=0;
-                for(k=0;k<4;k++){
-                    total+=bone1_CS[framecount][k][j]*bone2_CS[framecount][i][k];
-                }
-                bone12_CS[framecount][i][j]=total;
+    for(framecount=1;framecount<frame;framecount++){
+        double temp[3][3];
+        CoFactor(bone2_invrot[framecount],3,temp);
+        double det=matrix_det(bone2_invrot[framecount]);
+        transpose(temp,bone2_rot[framecount]);
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                bone2_rot[framecount][i][j]/=det;
             }
         }
     }
 
 
-    // Split the matrix into its respective rotation matrix and translation vector
     for(framecount=0;framecount<frame;framecount++){
-        for(i=0;i<4;i++)
-            for(j=0;j<4;j++){
-                if(i<3 && j<3){
-                    bone12_rot[framecount][i][j]=bone12_CS[framecount][i][j];
-                }
-                else if(j<3 && i==3){
-                    position12[framecount][j]=bone12_CS[framecount][i][j];
-                }
-            }
+        mult_matrix(bone2_rot[framecount],bone1_rot[framecount],bone12_rot[framecount]);
     }
 
+
+    for(framecount=0;framecount<frame;framecount++){
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                printf("%f\t",bone12_rot[framecount][i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n%d\n",framecount);
+    }
+
+    for(framecount=0;framecount<frame;framecount++){
+        for(j=0;j<3;j++){
+            position12[framecount][j]=position1[framecount][j]-position2[framecount][j];
+        }
+    }
+
+
     // At this point all data is loaded into memory in the appropriate format.
-    // Let's output the rotation angles of bone1 relative to bone2
-    
     for(i=0;i<count;i++){
-        icr(type,bone12_rot[(angles[2*i]-1)],bone12_rot[(angles[2*i+1]-1)],position12[(angles[2*i]-1)], position12[(angles[2*i+1]-1)],&phi,&screw_vec,&t,intersection);
+        //printf("\n\n%d\t%d",angles[2*i+1]-1,angles[2*i]-1);
+        icr(type,bone12_rot[(angles[2*i+1]-1)],bone12_rot[(angles[2*i]-1)],position12[(angles[2*i+1]-1)], position12[(angles[2*i]-1)],&phi,&screw_vec,&t,intersection);
 
         for(j=0;j<3;j++){
             results[i][j]=intersection[j];
         }
     }
     return results;
+}
+
+
+/*
+ *    Find the cofactor matrix of a square matrix
+ */
+
+void CoFactor(a,n,b)
+    double a[3][3];
+    int n;
+    double b[3][3];
+{
+    int i,j,ii,jj,i1,j1;
+    double det;
+    double **c;
+
+    c = malloc((n-1)*sizeof(double *));
+    for (i=0;i<n-1;i++)
+        c[i] = malloc((n-1)*sizeof(double));
+
+    for (j=0;j<n;j++) {
+        for (i=0;i<n;i++) {
+
+            /* Form the adjoint a_ij */
+            i1 = 0;
+            for (ii=0;ii<n;ii++) {
+                if (ii == i)
+                    continue;
+                j1 = 0;
+                for (jj=0;jj<n;jj++) {
+                    if (jj == j)
+                        continue;
+                    c[i1][j1] = a[ii][jj];
+                    j1++;
+                }
+                i1++;
+            }
+
+            /* Calculate the determinate */
+            det = Determinant(c,n-1);
+
+            /* Fill in the elements of the cofactor */
+            b[i][j] = pow(-1.0,i+j+2.0) * det;
+        }
+    }
+    for (i=0;i<n-1;i++)
+        free(c[i]);
+    free(c);
+}
+
+
+
+/*
+ *    Recursive definition of determinate using expansion by minors.
+ */
+double Determinant(double **a,int n)
+{
+    int i,j,j1,j2;
+    double det = 0;
+    double **m = NULL;
+
+    if (n < 1) { /* Error */
+
+    } else if (n == 1) { /* Shouldn't get used */
+        det = a[0][0];
+    } else if (n == 2) {
+        det = a[0][0] * a[1][1] - a[1][0] * a[0][1];
+    } else {
+        det = 0;
+        for (j1=0;j1<n;j1++) {
+            m = malloc((n-1)*sizeof(double *));
+            for (i=0;i<n-1;i++)
+                m[i] = malloc((n-1)*sizeof(double));
+            for (i=1;i<n;i++) {
+                j2 = 0;
+                for (j=0;j<n;j++) {
+                    if (j == j1)
+                        continue;
+                    m[i-1][j2] = a[i][j];
+                    j2++;
+                }
+            }
+            det += pow(-1.0,j1+2.0) * a[0][j1] * Determinant(m,n-1);
+            for (i=0;i<n-1;i++)
+                free(m[i]);
+            free(m);
+        }
+    }
+    return(det);
 }
